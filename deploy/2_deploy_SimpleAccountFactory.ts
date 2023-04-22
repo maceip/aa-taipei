fca9d74a8342809d7128e3f884c3c8454fd59a6c
rpm <ryan.macarthur@gmail.com>
2023-04-22 21:05:52 +0800

    dd

--- deploy/2_deploy_SimpleAccountFactory.ts
+++ deploy/2_deploy_SimpleAccountFactory.ts
@@ -5,8 +5,15 @@ import { ethers } from 'hardhat'
 const deploySimpleAccountFactory: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const provider = ethers.provider
   const from = await provider.getSigner().getAddress()
-
+  console.log("hee");
   const entrypoint = await hre.deployments.get('EntryPoint')
+  
+  const nft = await hre.ethers.getContractFactory("ERC721Token")
+  const [, alice, bob] = await hre.ethers.getSigners()
+
+  const BubblegumToken = await nft.deploy()
+
+
   const ret = await hre.deployments.deploy(
     'MechFactory', {
       from,
@@ -15,7 +22,15 @@ const deploySimpleAccountFactory: DeployFunction = async function (hre: HardhatR
       log: true,
       deterministicDeployment: true
     })
-  console.log('==mechFactory addr=', ret.address)
+
+    const meca = await ethers.getContractFactory('MechFactory');
+    const proxy = meca.attach(ret.address);
+    const generated = await proxy.callStatic.createAccount(BubblegumToken.address, 1, 999);
+    console.log(generated);
+
+
 }
+
+
 
 export default deploySimpleAccountFactory
